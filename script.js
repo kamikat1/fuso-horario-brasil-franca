fa8ed72f0c00475d652be3cebc72236380caa3e2
kamikat1 <88120995+kamikat1@users.noreply.github.com>
2022-07-01 13:42:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function FusoHorario() {
+    var inHoraBrasil = document.getElementById("inHoraBrasil");
+    var outHoraFranca = document.getElementById("outHoraFranca");
+    var horaBrasil = Number(inHoraBrasil.value);
+
+    if (inHoraBrasil == "" || isNaN(horaBrasil)) {
+        alert("Informe a hora do Brasil Corretamente");
+        inHoraBrasil.focus();
+        return;
+    }
+    var horaFranca = horaBrasil + 5;
+    horaFranca > 24 ? horaFranca = horaFranca - 24 : horaFranca;
+    outHoraFranca.textContent = "Hora na França: " + horaFranca.toFixed(2);
+}
+var btExibir = document.getElementById("btExibir");
+btExibir.addEventListener("click", FusoHorario);
+
+
+var texto = document.getElementById('outHoraFranca');
+texto.style.color = '#FF0000';
+body.style.backgroundColor = 'red';
